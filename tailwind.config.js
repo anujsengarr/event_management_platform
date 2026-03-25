9f9a60655d0415ebccc0ed39197885f5ea6f5e0c
Anuj Sengar <anujsengar2456@gmail.com>
2026-03-26 02:07:46 +0530

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "on-background": "#1a1c1b",
+                "background": "#f9f9f7",
+                "on-surface-variant": "#42493f",
+                "secondary-fixed-dim": "#95d68f",
+                "on-secondary-fixed": "#002204",
+                "on-primary": "#ffffff",
+                "surface-container-low": "#f4f4f2",
+                "surface-variant": "#e2e3e1",
+                "inverse-primary": "#a1d39a",
+                "surface": "#f9f9f7",
+                "tertiary-container": "#005639",
+                "on-tertiary-container": "#3fd298",
+                "primary": "#0e3c12",
+                "primary-container": "#275427",
+                "surface-container-lowest": "#ffffff",
+                "on-surface": "#1a1c1b",
+                "secondary-container": "#adf0a6",
+                "tertiary": "#003c27",
+                "surface-container-highest": "#e2e3e1",
+                "secondary": "#2e6b30",
+                "on-primary-container": "#95c78e",
+                "surface-container": "#eeeeec",
+                "outline": "#72796e",
+                "surface-bright": "#f9f9f7",
+                "error": "#ba1a1a",
+                "surface-container-high": "#e8e8e6",
+                "on-secondary": "#ffffff",
+                "tertiary-fixed": "#6ffbbe",
+                "tertiary-fixed-dim": "#4edea3",
+                "outline-variant": "rgba(194, 201, 188, 0.15)",
+            },
+            fontFamily: {
+                "headline": ["Inter", "sans-serif"],
+                "body": ["Inter", "sans-serif"],
+                "label": ["Inter", "sans-serif"]
+            },
+            borderRadius: {
+                "DEFAULT": "0.25rem",
+                "lg": "0.5rem",
+                "xl": "1.5rem",
+                "2xl": "2rem",
+                "full": "9999px"
+            },
+        },
+    },
+};
